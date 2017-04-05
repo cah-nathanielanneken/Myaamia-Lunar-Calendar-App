98e19c17318c58bc4b0a79b6d1ead2c26abe70eb
cah-nathanielanneken <annekent@miamioh.edu>
2017-04-05 16:49:33 -0400

    Updates

--- Myaamia-Calender-Static-HTML-Mockup/ParamaterYear/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include('LunarMonth.php');
-  $firstDayOfYear = "2017-01-29";
-  $date = "2017-01-30";
-  $isExtraMoon = False;
-  $removalDays = array();
-  $month = new LunarMonth($firstDayOfYear, $date, $isExtraMoon, $removalDays);
-  var_dump($month);
-?>
